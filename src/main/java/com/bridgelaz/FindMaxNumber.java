2150a3331d8a96d8698dbf0d36c949f6d2d3010c
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-29 10:41:09 +0530

    Write test code for max number fro first place

--- src/main/java/com/bridgelaz/FindMaxNumber.java
+++ src/main/java/com/bridgelaz/FindMaxNumber.java
@@ -1,8 +1,14 @@
 package com.bridgelaz;
 
 public class FindMaxNumber {
-    public static Integer checkMaxValue(int a, int b, int c) {
-
-        return a;
+    public static Integer checkMaxValue(Integer a, Integer b, Integer c) {
+        Integer maxNumber;
+        if(a.compareTo(b)>0 && a.compareTo(c)>0)
+            maxNumber=a;
+        else if(b.compareTo(a)>0&& b.compareTo(c)>0)
+            maxNumber=b;
+        else
+            maxNumber=c;
+        return maxNumber;
     }
 }
--- src/test/java/com/bridgelaz/UpdatedFindMax.java
+++ src/test/java/com/bridgelaz/UpdatedFindMax.java
@@ -8,10 +8,11 @@ public class UpdatedFindMax {
         @Test
         public void givenThreeIntegerNumber_ReturnMaximumNumber_FromFirstPlace(){
             FindMaxNumber findmax=new FindMaxNumber();
-            Integer maxInteger=findmax.checkMaxValue(10,80,30);
-            Assert.assertEquals(Integer.valueOf(10),maxInteger);
+            Integer maxInteger=findmax.checkMaxValue(80,10,30);
+            Assert.assertEquals(Integer.valueOf(80),maxInteger);
         }
-    }
+
+}
 
 
 
