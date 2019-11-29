b69fdafaab9080aa5214a1d507099070201ce3b4
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-29 10:45:00 +0530

    Add test Code for maxNumber at secound position

--- src/test/java/com/bridgelaz/UpdatedFindMax.java
+++ src/test/java/com/bridgelaz/UpdatedFindMax.java
@@ -11,6 +11,12 @@ public class UpdatedFindMax {
             Integer maxInteger=findmax.checkMaxValue(80,10,30);
             Assert.assertEquals(Integer.valueOf(80),maxInteger);
         }
+        @Test
+        public void givenThreeIntegerNumber_ReturnMaximumNumber_FromSecoundplace(){
+            FindMaxNumber findmax=new FindMaxNumber();
+            Integer maxInteger=findmax.checkMaxValue(10,50,30);
+            Assert.assertEquals(Integer.valueOf(50),maxInteger);
+    }
 
 }
 
