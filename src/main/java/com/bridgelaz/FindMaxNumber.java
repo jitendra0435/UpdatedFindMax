3006759fcbc63a37c97af92c8c7b83d726d93bea
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-29 10:34:33 +0530

    Write TestCode for max value from first place

---
+++ src/main/java/com/bridgelaz/FindMaxNumber.java
@@ -0,0 +1,8 @@
+package com.bridgelaz;
+
+public class FindMaxNumber {
+    public static Integer checkMaxValue(int a, int b, int c) {
+
+        return a;
+    }
+}
--- src/test/java/com/bridgelaz/UpdatedFindMax.java
+++ src/test/java/com/bridgelaz/UpdatedFindMax.java
@@ -1,6 +1,18 @@
 package com.bridgelaz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UpdatedFindMax {
 
+        @Test
+        public void givenThreeIntegerNumber_ReturnMaximumNumber_FromFirstPlace(){
+            FindMaxNumber findmax=new FindMaxNumber();
+            Integer maxInteger=findmax.checkMaxValue(10,80,30);
+            Assert.assertEquals(Integer.valueOf(10),maxInteger);
+        }
+    }
 
-}
+
+
+
